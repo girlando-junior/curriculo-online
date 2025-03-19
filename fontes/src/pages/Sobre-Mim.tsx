d677d99f2d3bcfbb8bf0f379b18fcad53df8611e
girlando-junior <girlandojunior209@gmail.com>
2025-03-18 22:34:02 -0300

    testando git

--- fontes/src/pages/Sobre-Mim.tsx
+++ fontes/src/pages/Sobre-Mim.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Sobre: React.FC = () => {
   return (
     <section id="sobre" className="h-screen flex items-center justify-center bg-gray-200">
-      <h2 className="text-4xl font-bold">Sobre Mim</h2>
+      <h2 className="text-4xl font-bold">Sobre Mi</h2>
     </section>
   );
 };
