f1ea0696c3d09b6e0ff862e8abfcb71dbec5f452
girlando-junior <girlandojunior209@gmail.com>
2025-06-21 17:21:14 -0300

    adicionando logo ao navbar

--- fontes/src/components/Sidebar.tsx
+++ fontes/src/components/Sidebar.tsx
@@ -20,30 +20,31 @@ const Navbar: React.FC<NavbarProps> = ({ logo, title, menuItems }) => {
   };
 
   return (
-    <div className="fixed top-0 left-0 bg-[var(--color-primary)] text-text w-full shadow-md">
-      <div className="mx-auto px-4 sm:px-5 lg:px-5">
-        <div className="flex justify-between items-center h-16">
-          <div className="flex-shrink-0">
-            {/* <h1 className="text-[var(--color-white)] text-xl font-bold">
-              {title}
-            </h1> */}
-            <img width={42} height={32} className="rounded-[8px]" src={logo} alt="logo" />
-          </div>
+    <div className="fixed top-0 left-0 bg-[var(--color-primary)] text-text w-full shadow-md p-[16px]">
+      <div className="flex justify-between items-center">
+        <div className="flex-shrink-0">
+          <img
+            width={42}
+            height={32}
+            className="rounded-[8px]"
+            src={logo}
+            alt="logo"
+          />
+        </div>
 
-          <div className="text-[var(--color-white)] flex space-x-4">
-            {menuItems.map((item, index) => (
-              <button
-                key={index}
-                onClick={() => scrollToSection(item.href.substring(1))} // Remove o "#" do href
-                className="relative px-3 py-2 text-sm font-medium transition duration-300 ease-in-out
+        <div className="text-[var(--color-white)] flex space-x-4">
+          {menuItems.map((item, index) => (
+            <button
+              key={index}
+              onClick={() => scrollToSection(item.href.substring(1))} // Remove o "#" do href
+              className="relative px-3 py-2 text-sm font-medium transition duration-300 ease-in-out
              after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:w-0 after:h-[2px]
              after:bg-[var(--color-primary-100)] after:transition-all after:duration-300 
              hover:after:w-full"
-              >
-                {item.label}
-              </button>
-            ))}
-          </div>
+            >
+              {item.label}
+            </button>
+          ))}
         </div>
       </div>
     </div>
